d71525558f1f0f6fd3849bd07f388dbf0e85a505
012 <168844475+012x010@users.noreply.github.com>
2024-05-21 14:39:21 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,15 +1,4 @@
 hr, min = map(int, input().split())
 timer = int(input())
 
-
-if(min+timer%60 >= 60) :
-    hr = hr+timer//60+1
-    min = min+timer%60-60
-else :
-    hr = hr+timer//60
-    min = min+timer%60
-
-if(hr>=24) :
-    hr = hr-24
-
-print(hr, min)
+print((hr+(timer//60)+(min+timer%60)//60)%24, (min+timer%60)%60)
