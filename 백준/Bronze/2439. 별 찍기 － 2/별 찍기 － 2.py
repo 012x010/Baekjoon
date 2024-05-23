f204af5a98e5566ce4ce7e3995d00ee16bf31a55
012 <168844475+012x010@users.noreply.github.com>
2024-05-23 14:40:31 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,11 +1,6 @@
 n = int(input())
+m = 0
 
-string = ""
-
-for i in range(n):
-    for j in range(n-i-1):
-        string+=" "
-    for j in range(i+1):
-        string+="*"
-    print(string)
-    string = ""
+for _ in range(n):
+    m += 1
+    print(" " * (n - m) + "*" * m)
