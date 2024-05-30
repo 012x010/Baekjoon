30de35115d79d213f6c07875429fd6419ee8f86f
012 <168844475+012x010@users.noreply.github.com>
2024-05-30 11:00:26 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+
+factor = []
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        factor.append(i)
+
+try:
+    print(factor[k - 1])
+except IndexError:
+    print("0")
